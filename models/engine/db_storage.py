05d11d7de4d327b1e577ad6ee4db1f5a2c55c131
MonaaEid <monaa_eid@hotmail.com>
2023-12-19 18:24:44 +0200

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,3 +17,5 @@ class DBStorage:
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                            format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
                            pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
