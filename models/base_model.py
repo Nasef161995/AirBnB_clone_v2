2728120551b55075d8051fd29d3b8a999c9c4474
Nasef161995~ <mnasef905@gmail.com>
2023-12-20 21:11:08 +0300

    aaa

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,22 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, DateTime, Column
 import models
-Base = declarative_base()
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    Base = declarative_base()
+else:
+    Base = object
+
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primar_key=True)
-    created_at = Column(DateTime, nullable=False, default= datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default= datetime.utcnow())
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -29,8 +38,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-                # del kwargs['__class__']
-                # self.__dict__.update(kwargs)
+            # del kwargs['__class__']
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -55,6 +64,6 @@ class BaseModel:
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
-    
+
     def delete(self):
         models.storage.delete(self)
