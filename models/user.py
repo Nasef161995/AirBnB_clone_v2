0bcc04fbe65aac9d0024b40fa6db1db56b8c5a23
Nasef161995~ <mnasef905@gmail.com>
2023-12-20 20:39:42 +0300

    aaa

--- models/user.py
+++ models/user.py
@@ -15,9 +15,13 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade='all, delete', backref="user")
+        reviews = relationship("Review", cascade='all, delete', backref="user")
+
+
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-    # places = relationship("Place", cascade='all, delete', backref="user")
+    
