8b881ea051d5c0b1a01cc6c73479f7e0553c5d6b
Nasef161995~ <mnasef905@gmail.com>
2023-12-20 16:47:58 +0300

    uuuu

--- models/user.py
+++ models/user.py
@@ -9,15 +9,11 @@ import sqlalchemy
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-    # places = relationship("Place", cascade='all, delete', backref="user")
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+
+    places = relationship("Place", cascade='delete', backref="user")
+    reviews = relationship("Review", cascade='delete', backref="user")
