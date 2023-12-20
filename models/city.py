8a086dd3912ac17f92fbaca5a997f40995e31a14
MonaaEid <monaa_eid@hotmail.com>
2023-12-20 04:18:13 +0200

    update

--- models/city.py
+++ models/city.py
@@ -3,11 +3,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
-
+import models
+from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        state_id = ""
+        name = ""
+
     # places = relationship("Place",  cascade='all, delete', backref="cities")
--- models/state.py
+++ models/state.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
-import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
-
-    @property
-    def cities(self):
-        my_list = []
-        my_dict = storage.all('City')
-        for key, value in my_dict.items():
-            if self.id == city.state_id:
-                my_list.append(value)
-        return my_list
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ""
+    if models.getenv("HBNB_TYPE_STORAGE")  != "db":
+        @property
+        def cities(self):
+            my_list = []
+            my_dict = models.storage.all('City')
+            for city, value in my_dict.items():
+                if self.id == city.state_id:
+                    my_list.append(value)
+            return my_list
