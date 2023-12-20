ba33df6b55cb1fb223865eaca2c07fa61ad22cf1
Nasef161995~ <mnasef905@gmail.com>
2023-12-21 00:47:02 +0300

    ffff

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,4 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
-                                   overlaps='place_amenities', viewonly=False)
+                                   viewonly=False)
