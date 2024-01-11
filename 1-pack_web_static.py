0e9185ca48d1175601202cedc0869db00adb4199
Nasef161995~ <mnasef905@gmail.com>
2024-01-12 02:46:36 +0300

    000

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""create a .tgz archive from contents of the web_static"""
+from fabric.api import local, task
+from datetime import datetime
+
+
+@task
+def do_pack():
+    """pack web_static folder"""
+    try:
+        cur_date = datetime.now().strftime('%Y%m%d%H%M%S')
+        arch = f'web_static_{cur_date}.tgz'
+        local('mkdir -p versions')
+        local(f'tar -cvzf versions/{arch} web_static')
+        return f'versions/{arch}'
+    except Exception:
+        return None
