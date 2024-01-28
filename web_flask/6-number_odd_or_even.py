be926ca7baa6afd4b4a80e1978dbcf8fafa35387
Nasef161995~ <mnasef905@gmail.com>
2024-01-29 00:05:14 +0300

    6666

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Flask web application"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_C(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def num(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_temp(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def num_type(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
