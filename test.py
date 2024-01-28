43e4561c0d2b6b5211fc4ea4c61079f53bfeea00
Nasef161995~ <mnasef905@gmail.com>
2024-01-28 19:32:45 +0300

    000

--- test.py
+++ test.py
@@ -3,13 +3,11 @@ from flask import Flask
 
 hello = Flask(__name__)
 
+
 @hello.route("/", strict_slashes=False)
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello HBNB!"
 
-@hello.route("/m")
-def hello_route():
-    return "<p> you are in /. route</p>"
 
 if __name__ == "__main__":
     hello.run(host="0.0.0.0", port=5000)
